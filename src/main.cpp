bc0e30534779f23d188f57a627b1c770820deff8
unknown <Aleks>
2018-11-08 22:30:27 +0700

    Начал дописывать класс дерева

--- src/main.cpp
+++ src/main.cpp
@@ -20,28 +20,40 @@ public:
 	}
 	~Tree(){}
 
-	void AddData(const string &type, const string &data)
+	bool AddData(const string &tegPrnt, const string &valPrnt, const string &tegChild, const string &valChild)
 	{
-		if(type == "int")
+		bool res = false;
+
+		if(tegChild == "int")
 		{
-			int d = stoi(data, 0);
+			int d = stoi(valChild, 0);
 			// convert string to int
-			Node *newNode = new Node(_INT_TYPE, &d);
+			Node *newNode = new Node(_TYPE_INT, &d);
 			this->rootNode = newNode;
+			res = true;
 		}
-		else if(type == "float")
+		else if(tegChild == "float")
 		{
-			float d = atof(data.c_str());
+			float d = atof(valChild.c_str());
 			// convert string to float
-			Node *newNode = new Node(_FLOAT_TYPE, &d);
+			Node *newNode = new Node(_TYPE_FLOAT, &d);
 			this->rootNode = newNode;
+			res = true;
 		}
-		else if(type == "string")
+		else if(tegChild == "string")
 		{
-			string d = data;
-			Node *newNode = new Node(_STRING_TYPE, &d);
+			string d = valChild;
+			Node *newNode = new Node(_TYPE_STRING, &d);
 			this->rootNode = newNode;
+			res = true;
 		}
+
+		return res;
+	}
+
+	void BFS(void *rootNode)
+	{
+
 	}
 
 	string getType()
@@ -52,15 +64,15 @@ public:
 		    return("null");
 		}
 
-		if(rootNode->dataType == _INT_TYPE)
+		if(rootNode->dataType == _TYPE_INT)
 		{
 			return("int");
 		}
-		else if(rootNode->dataType == _FLOAT_TYPE)
+		else if(rootNode->dataType == _TYPE_FLOAT)
 		{
 			return("float");
 		}
-		else if(rootNode->dataType == _STRING_TYPE)
+		else if(rootNode->dataType == _TYPE_STRING)
 		{
 			return("string");
 		}
@@ -71,11 +83,17 @@ public:
 	}
 
 private:
+
+	const string teg_int = "int";
+	const string teg_float = "float";
+	const string teg_string = "string";
+
 	enum DataType
 	{
-		_INT_TYPE,
-		_FLOAT_TYPE,
-		_STRING_TYPE
+		_TYPE_INT,
+		_TYPE_FLOAT,
+		_TYPE_STRING,
+		_TYPE_NULL
 	};
 
 	class Node
@@ -83,22 +101,23 @@ private:
 	public:
 		DataType dataType;
 		void *pData;
+		Node *pChild[3];
 
 		Node(DataType dt, void *pData)
 		{
 		    dataType = dt;
 
-		    if(dt == _INT_TYPE)
+		    if(dt == _TYPE_INT)
 			{
 				int i = *(int*)(pData);
 				this->pData = new int(i);
 			}
-			else if(dt == _FLOAT_TYPE)
+			else if(dt == _TYPE_FLOAT)
 			{
 				float f = *(float*)(pData);
 				this->pData = new float(f);
 			}
-			else if(dt == _FLOAT_TYPE)
+			else if(dt == _TYPE_FLOAT)
 			{
 				string s = *(string*)(pData);
 				this->pData = new string(s);
@@ -108,8 +127,51 @@ private:
 	private:
 	};
 
-	Node * rootNode;
+	Node* FindNode(Node *root, const string &teg, const string &val)
+	{
+		Node *pRes = nullptr;
 
+//		if()
+		{
+
+		}
+		return pRes;
+	}
+
+	bool CompareNodeData(Node *pNode, const string &teg, const string &val)
+	{
+		DataType dType = ConvertTegToDataType(teg);
+
+		if(pNode->dataType == dType)
+		{
+
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	DataType ConvertTegToDataType(const string &teg)
+	{
+		if(teg_int == teg)
+		{
+			return _TYPE_INT;
+		}
+		else if(teg_float == teg)
+		{
+			return _TYPE_FLOAT;
+		}
+		else if(teg_string == teg)
+		{
+			return _TYPE_STRING;
+		}
+		return _TYPE_NULL;
+	}
+
+	Node * rootNode;
 
 };
 
@@ -121,14 +183,19 @@ int main()
 //	float b = 3.14;
 	string s = "abc";
 
-	tree.AddData("int", "10");
+	tree.AddData("", "", "int", "10");
 	cout << "type=" << tree.getType() << endl;
 
-	tree.AddData("float", "3.14");
+	tree.AddData("", "", "float", "3.14");
 	cout << "type=" << tree.getType() << endl;
 
-	tree.AddData("string", "string");
+	tree.AddData("", "", "string", "string");
 	cout << "type=" << tree.getType() << endl;
+
+
+
+
+// --------------------------------------------
 
 	string filename = "tree.ser";
 
